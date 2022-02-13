11bf93f28bd43cd2357f6f3e3e56095910f15a4a
Cade Li <cadecode@foxmail.com>
2022-02-13 23:54:34 +0800

    feat: 添加相关 myql、redis 依赖，修改相关配置

--- distributed-lock-app/src/main/java/top/cadecode/app/DistributedLockDemoApp.java
+++ distributed-lock-app/src/main/java/top/cadecode/app/DistributedLockDemoApp.java
@@ -1,9 +1,11 @@
 package top.cadecode.app;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Cade Li
@@ -11,6 +13,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * @description 启动类
  */
 @SpringBootApplication
+@ComponentScan("top.cadecode")
+@MapperScan("top.cadecode.mysql")
 public class DistributedLockDemoApp extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(DistributedLockDemoApp.class, args);
